ef2fafe02b8263b347329a8aec35f0ed8175e7de
Sherman Chen <shermchen@paypal.com>
2018-06-22 08:39:35 -0700

    pettier now uses prettier to format lib

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = {
-  "printWidth": Math.floor((Math.random() * 120) + 1),
-  "useTabs": Math.random() > 0.5,
-  "tabWidth": Math.floor((Math.random() * 4) + 1),
-  "singleQuote": Math.random() > 0.5,
-  "trailingComma": Math.random() > 0.5 ? "es5" : "all",
-  "arrowParens": Math.random() > 0.5 ? "avoid" : "always",
-  "bracketSpacing": Math.random() > 0.5,
-  "semi": Math.random() > 0.5,
-  "jsxBracketSameLine": Math.random() > 0.5
-}
+  printWidth: Math.floor(Math.random() * 120 + 1),
+  useTabs: Math.random() > 0.5,
+  tabWidth: Math.floor(Math.random() * 4 + 1),
+  singleQuote: Math.random() > 0.5,
+  trailingComma: Math.random() > 0.5 ? "es5" : "all",
+  arrowParens: Math.random() > 0.5 ? "avoid" : "always",
+  bracketSpacing: Math.random() > 0.5,
+  semi: Math.random() > 0.5,
+  jsxBracketSameLine: Math.random() > 0.5
+};
